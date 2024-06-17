71a763c5312466f1dccdd6e0dca9a6a355456a8b
abrahaamv <velizabrahaam@gmail.com>
2024-06-17 09:00:30 -0600

    Added necesary configs  to deploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,27 +1,20 @@
 <?php
-
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
-
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
      * Bootstrap any application services.
+     *
+     * @return void
      */
-    public function boot()
-   {
-    if (env('APP_ENV') === 'production') {
-        URL::forceScheme('https');
+    public function boot(UrlGenerator $url)
+    {
+        if(env('APP_ENV') !== 'local')
+        {
+            $url->forceSchema('https');
+        }
     }
-}
 }
