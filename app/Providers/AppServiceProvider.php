e59768e89642caf898f77f8f3989034dbc3ff2a5
abrahaamv <velizabrahaam@gmail.com>
2024-06-17 08:37:43 -0600

    Added necesary config  to deploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,4 +22,11 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
+
+    public function boot()
+   {
+    if (env('APP_ENV') === 'production') {
+        URL::forceScheme('https');
+    }
+}
 }
