d266a162c5df404c54717ea402db1f860d80d10e
abrahaamv <velizabrahaam@gmail.com>
2024-06-17 09:13:16 -0600

    Forzar HTTPS en URLs de recursos

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,20 +1,31 @@
 <?php
+
 namespace App\Providers;
 
-use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
     /**
      * Bootstrap any application services.
      *
      * @return void
      */
-    public function boot(UrlGenerator $url)
+    public function boot()
     {
-        if(env('APP_ENV') !== 'local')
-        {
-            $url->forceSchema('https');
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
         }
     }
 }
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,8 +1,16 @@
+<IfModule mod_rewrite.c>
+    RewriteEngine On
+    RewriteCond %{HTTPS} !=on
+    RewriteRule ^ https://%{HTTP_HOST}%{REQUEST_URI} [L,R=301]
+</IfModule>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ secure_asset('build/assets/app-Cp6YETGy.css') }}">
+    <script src="{{ secure_asset('build/assets/app-GsZkvmek.js') }}"></script>
+
     @viteReactRefresh
     @vite('resources/js/app.jsx')
     @inertiaHead
